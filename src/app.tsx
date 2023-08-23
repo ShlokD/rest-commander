6a0ca46e5d75455127936e849a886470b67c9913
Shlok Desai <shlokpdesai@gmail.com>
2023-08-23 16:01:51 +0530

    Fix styling

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,9 @@ import RequestPane from "./request-pane";
 export function App() {
   return (
     <div className="flex flex-col w-full min-h-screen bg-black text-gray-400 p-4">
-      <header className="p-6 w-full flex items-center gap-2 bg-gray-800 rounded-lg self-center">
+      <header className="p-6 w-full flex items-center gap-2 bg-gray-800 rounded-lg self-center justify-center">
         <img src="/logo.png" height="48" width="48" alt="" />
-        <h1 className="font-bold text-xl">Rest Commander</h1>
+        <h1 className="font-bold text-4xl text-white">Rest Commander</h1>
       </header>
       <main className="flex flex-col w-full min-h-screen">
         <RequestPane />
